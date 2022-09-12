1aa12363ba38339d327322c967cdb5af37d5a213
BenjaminGrabow <codingbenjamingrabow@gmx.de>
2022-09-12 11:46:20 +0200

    feat: update address

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -20,5 +20,5 @@ export const mainnetInfuraNode = 'https://mainnet.infura.io/v3/4eadc04c11494a3e8
 
 export const airdropAbi = Airdrop.abi;
 
-export const nftContractAddress = '0x47a3f6F24E9b34D21387fAf29AC0832b5852F582';
+export const nftContractAddress = '0x90b3812A2b94C97c4d058d948FED9e65CB55606d';
 export const airdropContractAddress = '0xA8ee5252Ad4Be60c1e1D2A5D8BA005b6d8EFCECF';
